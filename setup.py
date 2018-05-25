aa90ca6753bb316680fcf753e484fd8420efc4d0
Dylan Sather <dylan.sather@gmail.com>
2018-05-25 12:43:05 -0700

    Modifying setup.py to include long description, etc.

--- setup.py
+++ setup.py
@@ -1,16 +1,33 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="awsbudget",
-    version='0.0.1',
+    description='Tool to manage a single, simple AWS Budget',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords=['aws', 'budget'],
+    version='0.0.3',
+    author='Dylan Sather',
+    author_email='dylan.sather@gmail.com',
+    url='https://github.com/dylburger/aws-budgets-cli',
+    license='MIT',
+    python_requires='>=3',
     py_modules=['awsbudget'],
     packages=find_packages(),
     install_requires=[
         'boto3',
-        'Click',
+        'click',
     ],
     entry_points='''
         [console_scripts]
         awsbudget=awsbudget:cli
     ''',
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+    )
 )
